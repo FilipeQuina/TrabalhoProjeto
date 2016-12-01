66f9a8cd2e64030d766630dcabf7d17e20bafc94
Guilherme <souzasiguilherme@gmail.com>
2016-12-01 10:28:33 -0200

    Strategy + início do main

---
+++ Locadora/src/java/locadora/model/CalculoImpostoVinteOuQuinze.java
@@ -0,0 +1,12 @@
+package locadora.model;
+
+
+public class CalculoImpostoVinteOuQuinze implements CalculaImposto {
+	@Override
+	public double calculaSalarioComImposto(Funcionario umFuncionario) {
+		if (umFuncionario.getSalarioBase() > 3500) {
+			return umFuncionario.getSalarioBase() * 0.8;
+		}
+		return umFuncionario.getSalarioBase() * 0.85;
+	}
+}
--- Locadora/src/java/locadora/model/Funcionario.java
+++ Locadora/src/java/locadora/model/Funcionario.java
@@ -17,12 +17,47 @@ import javax.persistence.Id;
  */
 
 public class Funcionario {
+    public static final int PADRAO = 1;
+    public static final int GERENTE = 2;
+    public static final int SECRETARIO = 3;
     private Long id;
     private String nome;
     private String senha;
+    protected double salarioBase;
+    protected int cargo;
+    protected CalculaImposto estrategiaDeCalculo;
 
     
-    
+    	public Funcionario(int cargo, double salarioBase, Long id, String nome, String senha) {
+		this.salarioBase = salarioBase;
+                this.nome = nome;
+                this.id = id;
+                this.senha = senha;
+		switch (cargo) {
+		case PADRAO:
+			estrategiaDeCalculo = new CalculoImpostoQuinzeOuDez();
+			cargo = PADRAO;
+			break;
+		case SECRETARIO:
+			estrategiaDeCalculo = new CalculoImpostoQuinzeOuDez();
+			cargo = SECRETARIO;
+			break;
+		case GERENTE:
+			estrategiaDeCalculo = new CalculoImpostoVinteOuQuinze();
+			cargo = GERENTE;
+			break;
+		default:
+			throw new RuntimeException("Cargo nao encontrado!");
+		}
+	}
+
+	public double calcularSalarioComImposto() {
+		return estrategiaDeCalculo.calculaSalarioComImposto(this);
+	}
+
+	public double getSalarioBase() {
+		return salarioBase;
+	}
     
     public Long getId() {
         return id;
